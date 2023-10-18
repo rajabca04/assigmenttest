7c162db9c7483daa3cf7d0a4adff7aa5069f5ce0
rajabca04 <rajabca201804@gmail.com>
2023-10-18 17:10:23 +0530

    Disable right click

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 
 import './App.css';
+import React, { useEffect } from 'react';
 // import Chartexp from './components/chartjs/Chartexp';
 import Axiousexp from './components/chartjs/Axiousexp';
 
 function App() {
+  useEffect(() => {
+    const handleContextmenu = e => {
+        e.preventDefault()
+    }
+    document.addEventListener('contextmenu', handleContextmenu)
+    return function cleanup() {
+        document.removeEventListener('contextmenu', handleContextmenu)
+    }
+}, [ ])
   return (
     <div className="App">
       {/* <Chartexp/> */}
